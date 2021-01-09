00f9dc482218493903e74f40ca1166c43864940f
aelkin <daselkin@gmail.com>
2021-01-09 21:17:09 +0200

    Removed redundant files

--- audio.py
+++
@@ -1,8 +0,0 @@
-from audacious import AudaciousController
-from pactl import PulseAudioController
-import logging
-
-class AudioController(PulseAudioController, AudaciousController):
-	def __init__(self):
-		logging.debug("Setting up AudioController class")
-
