43b2b4a868900457d26d979a5b76051dc0128598
aelkin <daselkin@gmail.com>
2021-01-24 22:16:42 +0200

    Added playlist and track classes

--- playback_handlers/audio.py
+++ playback_handlers/audio.py
@@ -1,8 +1,20 @@
 from .audacious import AudaciousController
 from .pactl import PulseAudioController
+from .playlist import Playlist
+
+from util.patterns import Borg
+
 import logging
 
-class AudioController(PulseAudioController, AudaciousController):
+class AudioController(Borg, PulseAudioController, AudaciousController):
 	def __init__(self):
-		logging.debug("Setting up AudioController class")
+		Borg.__init__(self)
 
+
+	def _init(self):
+		logging.debug("Setting up AudioController class")
+		self.playlist = Playlist()
+
+	def load_file(self, file_name: str, enqueue: bool = False) -> None:
+		AudaciousController.load_file(file_name)
+		self.playlist.set_file_or_directory(file_name, enqueue)
---
+++ playback_handlers/playlist.py
@@ -0,0 +1,58 @@
+import os
+
+class Track():
+	def __init__(self, filename):
+		self.filename = filename
+		self.display_name = os.path.basename(filename)
+
+/
+class Playlist():
+	def __init__(self):
+		self.clear()
+
+
+	def clear(self):
+		self.name = None
+		self.tracks = []
+		self.position = None		
+
+
+	def add_file(self, filename: str) -> None:
+		assert os.path.isfile(filename)
+		except AssertionError:
+			raise FileNotFoundError(f"File {filename} not found.") 
+		self.tracks.append(Track(filename))
+
+
+	def add_directory(self, directory_name: str) -> None:
+		assert os.path.isdir(directory_name)
+		except AssertionError:
+			raise FileNotFoundError(f"Directory {directory_name} not found.") 
+
+		for (dirpath, dirnames, filenames) in os.walk(directory_name):
+			for filename in file_names:
+				add_file(os.path.join(dirpath, filename))
+
+
+	def set_file(self, filename: str) -> None:
+		self.clear()
+		self.add_file(filename)
+		self.name = self.tracks[0].display_name
+
+
+	def set_directory(self, directory_name: str) -> None:
+		self.clear()
+		self.add_directory(filename)
+		self.name = os.path.basename(directory_name)
+
+
+	def add_file_or_directory(self, path: str, enqueue: bool) -> None:
+		assert os.path.exists(path)
+		except AssertionError:
+			raise FileNotFoundError(f"Path {path} not found.")
+		if os.isfile(path):
+			(self.add_file if enqueue else self.set_file)(path)
+		elif os.isdir(path):
+			(self.add_directory if enqueue else self.set_directory)(path)
+		else:
+			raise FileNotFoundError(f"{path} is neither file nor directory")
